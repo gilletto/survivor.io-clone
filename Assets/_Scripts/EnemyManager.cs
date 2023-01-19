7ce498afbc40471b20f71bc87581159767303450
Alessandro Gilardoni <axelett@gmail.com>
2023-01-19 17:54:46 +0100

    Remove old function

--- Assets/_Scripts/EnemyManager.cs
+++ Assets/_Scripts/EnemyManager.cs
@@ -13,90 +13,18 @@ public class EnemyManager : MonoBehaviour
     [SerializeField] float _totalEnemiesPerWave;
     [SerializeField] GameObject _player;
     List<Enemy> _enemies = new List<Enemy>();
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+   
     // Update is called once per frame
     void Update()
     {
         // spawn a fixed number of enemy
-        while (Enemy.Enemycount < 10)
+        if (Enemy.Enemycount < 10)
         {
             Debug.Log("Enemy count" + Enemy.Enemycount);
             SpawnEnemy();
         }
-
-        // search for nearby enemies
-        //foreach(Enemy enemy in _enemies)
-        //{
-        //    List<Transform> context = GetNearbyObjects(enemy);
-
-        //    //Debugging avoidance radius : red too nearby enemy, white 0 nearby enemy
-        //    SpriteRenderer[] renderers = enemy.GetComponentsInChildren<SpriteRenderer>();
-        //    foreach (SpriteRenderer rend in renderers)
-        //    {
-        //        rend.color = Color.Lerp(Color.white, Color.red, context.Count / 6f);
-        //    }
-
-        //    Vector2 avoidanceMovement = Vector2.zero;
-        //    int nAvoid = 0;
-
-        //    foreach(Transform item in context)
-        //    {
-        //        // check if enemy is inside avoidanceCircle
-        //        if (Vector2.SqrMagnitude(item.position - enemy.transform.position) < enemy.AvoidanceRadius){
-        //            // add  number of element to avoid
-        //            // get direction from nearby enemy or enemies
-        //            // invert direction
-        //            // set movement
-        //            nAvoid++;
-        //            var directionToNearbyEnemy = item.position - enemy.transform.position;
-        //            var inverseDirection = -directionToNearbyEnemy;
-        //            avoidanceMovement += (Vector2)inverseDirection;
-        //        }
-         
-        //    }
-        //    // divide movement by number of element to avoid to get smooth movement
-        //    if(nAvoid > 0)
-        //    {
-        //        avoidanceMovement /= nAvoid;
-        //    }
-        //    // set enemy avoidance movement
-        //    enemy.SetAvoidance(avoidanceMovement);
-       
-        //}
-
-        // compute number of enemies to spawn
-        //SpawnWave();
-
-
     }
 
-    /// <summary>
-    ///  Search for nearby enemies
-    /// </summary>
-    /// <returns></returns>
-    private List<Transform> GetNearbyObjects(Enemy enemy)
-    {
-        List<Transform> context = new List<Transform>();
-        Collider2D[] contextColliders = Physics2D.OverlapCircleAll(enemy.transform.position, enemy.AvoidanceRadius);
-        foreach (Collider2D c in contextColliders)
-        {
-            if (c != enemy.EnemyCollider)
-            {
-                context.Add(c.transform);
-            }
-        }
-        return context;
-    }
-
-    private void SpawnWave()
-    {
-       
-    }
 
     private void SpawnEnemy()
     {
